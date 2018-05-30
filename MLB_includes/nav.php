7502e9b79f2805175e012d7fea67705bb52d7e6d
mcvignault <34689814+mcvignault@users.noreply.github.com>
2018-05-30 10:43:58 -0400

    Update nav.php

--- MLB_includes/nav.php
+++ MLB_includes/nav.php
@@ -2,7 +2,7 @@
 	<li><a href="index.php">Homepage</a></li>
 	<li><a href="USI_Rules.php">Rules</a></li>
 	<li><a href="MLB_Registration.php">Registration</a></li>
-	<li><a href="MLB_Games2.php">Picks</a></li>
+	<li><a href="MLB_Games2.php">Make Picks</a></li>
 	<li><a href="Standings_Main.php">Standings</a></li>
 	<li><a href="Contact.php">Contact</a></li>
 </ul>

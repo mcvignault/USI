829552b8091a5065dfd10c2e3e14b00ffcb9027b
mcvignault <34689814+mcvignault@users.noreply.github.com>
2018-05-30 10:46:54 -0400

    Update USI_Rules.php

--- USI_Rules.php
+++ USI_Rules.php
@@ -15,23 +15,23 @@
 								</header>
 								<p><a href="#" class="image full"><img src="images/referees2.jpg" width="1000" height="200" alt=""></a></p>
 								<h2>REGISTRATION REQUIREMENTS</h2>
-								<p>Each player must have a personal account at DraftKings.</br>
-If you do not have an account, please utilize our referral link when registering by clicking <a href="#">here</a>.</br> 
-
+								<p>Each player must have a personal account at DraftKings.</p>
+<p>If you do not have an account, please utilize our referral link when registering by clicking <a href="#">here</a>. </p>
+<p>
 In order to create an account, DraftKings requires the following:
 <li>your full name</li>
 <li>a valid e-mail address</li>
 <li>province and country of residence</li>
 <li>date of birth (to confirm you are of legal age)</li>
-</br>
+</p>
 <p>For the purposes of this competition, you do not need to deposit any money into the account; however, if you choose to deposit $5 or more, you will receive a sign-up bonus. This bonus is only available to new users who use the referral link above; if you already have an account, please do not try to create a second one, as this is strictly forbidden by DraftKings' TOS and will result in a permanent ban from the site.</p>
 <h2>DAILY FANTASY SPORTS RULES</h2>
 <p>For a summary of DraftKings scoring and rules, please click <a href="https://www.draftkings.com/help/rules/mlb">here</a>.</p>
 <p>
-DraftKings contests will be available via the links on the "Competition" page the night before the day of the competition.<br />
-
-Each competitor must submit a lineup (through the DraftKings website or mobile app) by 7 p.m.<br />
-
+DraftKings contests will be available via the links on the <a href="MLB_Games2.php">Make Picks</a> page the night before the day of the competition.</p>
+<p>
+Each competitor must submit a lineup (through the DraftKings website or mobile app) by 7 p.m.</p>
+<p>
 It is strongly recommended that users download the free DraftKings mobile app (available for iOS and Android), which will provide notifications when an invitation has been received, and allow users to follow their contests throughout the evening.
 </p>
 <p>
@@ -54,13 +54,13 @@ ii.) Betting options are as follows:
 <li>Totals (total points scored by both teams - O/U</li>
 </p>
 
-<p>iii.) The betting lines will be available in the "Competition" tab by noon on the days of contests.
+<p>iii.) The betting lines will be available in the <a href="MLB_Games2.php">Make Picks</a> page by noon on the days of contests.
 ​
 It is each participants' responsibility to submit their selections by 7 p.m. on the night of the competition.
 
 No bets will be accepted once the evening's games have begun.
 </p>
-<p>iv.) In order to encourage smart bankroll management, as well as to prolong the competition, participants will be limited to betting a maximum of 25% of their bankroll each night.
+<p>iv.) In order to encourage smart bankroll management, as well as to prolong the competition, participants will be limited to betting a maximum of 25% of their bankroll each night.<br />
 
 For example, each competitor will have a $20 bankroll on the first night. That means that the most a user can bet on the first night is $5 (i.e., $20 x 25%).
 If the competitor's bankroll increases to $30 by night two, they will then be able to place a maximum bet of $7.50 (i.e., $30 x 25%).
@@ -69,11 +69,11 @@ If the competitor's bankroll increases to $30 by night two, they will then be ab
 <p>v.) Points will be awarded in the same fashion as they are in the daily fantasy sports contests.
 
 Once all games are complete for the evening, participants will be ranked based on their earnings that night. Points will again be awarded as follows
-<ul>1st Place: 5 points</ul>
-<ul>2nd Place: 4 points</ul>
-<ul>3rd Place: 3 points</ul>
-<ul>4th Place: 2 points</ul>
-<ul>5th Place: 1 point</ul>
+<li>1st Place: 5 points</li>
+<li>2nd Place: 4 points</li>
+<li>3rd Place: 3 points</li>
+<li>4th Place: 2 points</li>
+<li>5th Place: 1 point</li>
 </p>
 <p>
 vi.) In addition to the overall prize pool, participants will be given the opportunity to receive prizes equal to that of any betting profits made throughout the competition. For example, if a competitor ends the competition with a bankroll of $50, they will receive a prize of $30 
@@ -82,9 +82,9 @@ vi.) In addition to the overall prize pool, participants will be given the oppor
 <h2>DFS ELIMINATION</h2>
 i) Each competition night a separate DFS contest will be held in which the last place finisher will be eliminated.
 </br></br>
-ii) The elimination pool will run simultaneously with the main Ultimate Sports Investor competition, however you are not required to enter the same lineup in each contest.  Draft Kings allows you to draft as many lineups as you want in a night, therefore if you choose, you have the opportunity to draft different lineups to enter into the main contest and the elimination pool.  Of course, you may enter the same lineup into both as well.
+ii) The Eliminator will run simultaneously with the main Ultimate Sports Investor competition, however you are not required to enter the same lineup in each contest.  DraftKings allows you to draft as many lineups as you want in a night, therefore if you choose, you have the opportunity to draft different lineups to enter into the Main contest and the Eliminator contest.  Of course, you may enter the same lineup into both as well.
 </br></br>
-iii) Registration into the elimination pool is not limited, however the total number of participants will be split into individual pools with a maximum of 11 participants such that one winner will remain at the end of 10 nights.  If pools of 11 people are not achieved (i.e. 9 people), Sports [TR]en[D]gineering will designate certain nights of the competition as elimination pool nights (i.e. if 9 people are in the pool, 8 of the 10 nights will be elimination pool nights).
+iii) Registration into the Eliminator is not limited, however the total number of participants will be split into individual pools with a maximum of 11 participants such that one winner will remain at the end of 10 nights.  If pools of 11 people are not achieved (i.e. 9 people), Trend-Eng Sports will designate certain nights of the competition as Eliminator nights (i.e. if 9 people are in the pool, 8 of the 10 nights will be Eliminator nights).
 </br></br>
 iv) The pool will be a winner-take-all prize pool.
 </section>

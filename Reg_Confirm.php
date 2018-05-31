fe58eb7f7465162f6b4f91c5821fc4714bf9a8df
mcvignault <34689814+mcvignault@users.noreply.github.com>
2018-05-31 10:51:33 -0400

    Add files via upload

---
+++ Reg_Confirm.php
@@ -0,0 +1,33 @@
+<?php include('MLB_includes/USI_header.php');
+?>
+		
+	<!-- Main -->
+		<div id="main">
+			<div class="container">
+				<div class="row">
+
+					<!-- Content -->
+						<div id="content" class="12u skel-cell-important">
+							<section>
+								<header>
+									
+									<span class="byline">
+										<p>
+											<h2>Thank you for registering!  An email has been sent to the email address you provided.  Please follow the steps provided to complete your registration.</h2>"
+											</p>
+											<hr />
+									</span>
+								</header>								
+</section>
+						</div>
+					<!-- /Content -->
+						
+				</div>
+			
+			</div>
+		</div>
+	<!-- Main -->
+
+	<!-- Footer -->
+		<?php include('MLB_includes/USI_footer.php');
+	?>
